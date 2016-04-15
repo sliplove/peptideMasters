e696f8815d88efbc0668676381c4fc9ed2b816f3
Anton Korobeynikov <anton@korobeynikov.info>
2016-04-15 03:03:26 +0300

    Cleanup

--- peptide_gen.R
+++ peptide_gen.R
@@ -18,15 +18,15 @@ max.W = 100
 sz <- 8
 
 ilya <- c(296.089,  324.153,  327.999,  338.181,  341.846,  359.415,  366.881,
-             372.44,  386.08,   395.07,   423.098,  433.021,  437.202,  441.282, 
-             455.124,  462.093,  464.852,  481.362,  483.64,   508.513,  533.283, 
+             372.44,  386.08,   395.07,   423.098,  433.021,  437.202,  441.282,
+             455.124,  462.093,  464.852,  481.362,  483.64,   508.513,  533.283,
              536.238,  540.697,  549.837, 554.36,   568.234,  569.96,   581.238,
              596.306,  616.903,  631.247,  637.379,  649.104,  667.163,  681.25,
              685.378,  686.424,  698.233,  722.808,  735.46,  746.496,  751.526,
-             752.401,  764.41,   780.184,  782.373,  816.009,  820.065,  827.28,  
-             842.327,  847.555,  848.771,  849.451,  877.359,  893.387,  895.455, 
-             909.388,  913.777,  933.373,  939.125,  951.488,  959.293,  988.316, 
-             989.367) 
+             752.401,  764.41,   780.184,  782.373,  816.009,  820.065,  827.28,
+             842.327,  847.555,  848.771,  849.451,  877.359,  893.387,  895.455,
+             909.388,  913.777,  933.373,  939.125,  951.488,  959.293,  988.316,
+             989.367)
 
 modify.mass <- function(mass) {
   id <- sample(1:sz, 1)
@@ -39,10 +39,10 @@ modify.mass <- function(mass) {
   return(new.mass)
 }
 
-get.score <- function(mass) 
+get.score <- function(mass)
   get_score(sort(ilya), sort(mat%*%mass))
 
-weight.spector <- function(score, s.min = 0) 
+weight.spector <- function(score, s.min = 0)
   log(score - s.min + 1)
 
 mh.update <- function(mass, score, lweight.spector) {
@@ -51,10 +51,10 @@ mh.update <- function(mass, score, lweight.spector) {
   if(new.score > MAX_SCORE) {
     new.score <- MAX_SCORE
   }
-  
+
   lold.weight <- lweight.spector(score)
   lnew.weight <- lweight.spector(new.score)
-  
+
   alpha <- min(1, exp(lnew.weight - lold.weight))
   if (runif(1) < alpha) {
     score <- new.score
@@ -76,18 +76,18 @@ mh.usual <- function(N) {
     if(l$score > MAX_SCORE) {
       l$score <- MAX_SCORE
     }
-    
+
     v[i] <- l$score
     i <- i + 1
   }
-  
+
   v
 }
 
 mh.weighted <- function(N, w, s.min, trace = TRUE) {
-  weight.w <- function(score) { 
+  weight.w <- function(score) {
     w[score - s.min + 1]}
-  
+
   i <- 1
   v <- numeric(N)
   l <- list(mass = start.mass, score = score)
@@ -116,14 +116,14 @@ wl.step <- function(s.min, s.max, phi,
   weight.wl <- function(score) {
     w[score - s.min + 1]
     }
-  
+
   tmp <- sample(1:max.W, 8, replace = TRUE)
   start.mass <- (tmp/sum(tmp)*TOTAL_MASS)
   score <- get.score(start.mass)
-  
+
   #temp!
   if (score > MAX_SCORE) {
-    score <- MAX_SCORE 
+    score <- MAX_SCORE
   }
   l <- list(mass = start.mass, score = score)
   i <- 1
@@ -133,7 +133,7 @@ wl.step <- function(s.min, s.max, phi,
     if(l$score > MAX_SCORE) {
       l$score <- MAX_SCORE
     }
-    
+
     idx <- l$score - s.min + 1
     h[idx] <- h[idx] + 1
     w[idx] <- w[idx] - lphi
@@ -159,9 +159,9 @@ wl <- function(s.min, s.max,
                thr = 100500,
                trace = TRUE) {
   phi <- phi.start
-  
+
   if (trace) cat(sprintf("wl step: phi=%f\n", phi))
-  ll <- wl.step(s.min, s.max, phi, thr = thr, trace = trace)  
+  ll <- wl.step(s.min, s.max, phi, thr = thr, trace = trace)
   while (phi > phi.end) {
     phi <- sqrt(phi)
     if (trace) cat(sprintf("wl step: phi=%f\n", phi))
@@ -170,11 +170,11 @@ wl <- function(s.min, s.max,
   ll$w
 }
 #-----------------------------------------------------------------------------------------------------------------------
-#test if everything is correct 
+#test if everything is correct
 weights <- wl(0, MAX_SCORE)
 s.min <- 0
 ww <- exp(weights)
-L <- 1000000      
+L <- 1000000
 one.traj <- mh.weighted(L, s.min = 0, w = weights)
 one.traj
 plot(cumsum(one.traj < 10)/1:L, lwd = 3, ty = "l", xlab = "number of simulations", ylab = "Cumulative mean")
