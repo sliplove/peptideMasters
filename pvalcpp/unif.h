3aa6cd6c7c94b10def5aa041d505a9efef82aa25
sliplove <abramova.asya93@gmail.com>
2017-01-23 15:20:08 +0300

    change uniform generator

--- pvalcpp/unif.h
+++ pvalcpp/unif.h
@@ -1,10 +1,23 @@
 #pragma once 
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <random>
+#include <cmath>
+#include "../include/pcg_random.hpp"
 
-static double unif_rand() {
-	return double(rand()) / RAND_MAX;
+static double unif_real(pcg_extras::seed_seq_from<std::random_device> & seed_source, double a, double b) {
+    pcg32 rng(seed_source);
+    std::uniform_real_distribution<> dis(a, b);
+    double rval = dis(rng);   
+    return rval;
 }
 
 
-static double unif(double a, double b) {
-	return unif_rand() * (b - a) + a;
+static int unif_int(pcg_extras::seed_seq_from<std::random_device> & seed_source, int a, int b) {
+    pcg32 rng(seed_source);
+    std::uniform_int_distribution<> dis(a, b);
+    double rval = dis(rng);   
+    return rval;
 }
